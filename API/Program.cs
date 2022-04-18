4f1c8804492777282cf30f895312595fd2100401
termeh <termehtabatabaee@yahoo.com>
2022-04-18 22:08:30 +0430

    adding Configurations

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,18 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope()){
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>;
+    try{
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch(Exception ex){
+        
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -29,3 +41,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
