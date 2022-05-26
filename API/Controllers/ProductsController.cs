e2848cb0d69e4dcbdfe8f241976c38d5270fa25e
termeh <termehtabatabaee@yahoo.com>
2022-05-26 17:56:20 +0430

    data shaped

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
 
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifcation;
@@ -16,9 +18,11 @@ public class ProductsController : ControllerBase
         private readonly IGenericRepository<ProductType> _typeRepo;
         private readonly IGenericRepository<ProductBrand> _brandRepo;
         private readonly IGenericRepository<Product> _productRepo;
+        private readonly IMapper _mapper;
 
-    public ProductsController(IGenericRepository<Product> productRepo, IGenericRepository<ProductBrand> brandRepo, IGenericRepository<ProductType> typeRepo)
+    public ProductsController(IGenericRepository<Product> productRepo, IGenericRepository<ProductBrand> brandRepo, IGenericRepository<ProductType> typeRepo, IMapper mapper)
     {
+            _mapper = mapper;
             _productRepo = productRepo;
             _brandRepo = brandRepo;
             _typeRepo = typeRepo;
@@ -26,18 +30,19 @@ public class ProductsController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<List<Product>>> GetProducts()
+    public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
     {
         var spec = new ProoductsWithTypesAndBrandSpecification();
         var products = await _productRepo.ListAsync(spec);
-        return Ok(products);
+        return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
     }
     
     [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetProduct(int id){
+    public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id){
 
         var spec = new ProoductsWithTypesAndBrandSpecification(id);
-        return await _productRepo.GetEntityWithSpec(spec);
+        var product = await _productRepo.GetEntityWithSpec(spec);
+        return _mapper.Map<Product, ProductToReturnDto>(product);
 
     }
 
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
 
 var app = builder.Build();
 
@@ -37,6 +39,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
