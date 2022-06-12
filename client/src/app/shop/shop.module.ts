1f4e4f79fba70f3dddd37e75386357789c971ced
termeh <termehtabatabaee@yahoo.com>
2022-06-13 03:14:41 +0430

    added routing links

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -10,11 +12,13 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     ShopComponent,
     ProductItemComponent,
+    ProductDetailsComponent,
     
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
   exports: [
     ShopComponent
