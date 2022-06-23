73b09f999380f5b9b9e7242b2eac933e6cc8d62b
termeh <termehtabatabaee@yahoo.com>
2022-06-24 03:39:22 +0430

    added theme and features

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,11 +6,12 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { Toast, ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule,
@@ -20,7 +21,8 @@ import { Toast, ToastrModule } from 'ngx-toastr';
     }) 
   ],
   exports:[
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }
