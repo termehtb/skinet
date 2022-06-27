15bfa1034098595e42a48ebc1ffb8184e73afb70
termeh <termehtabatabaee@yahoo.com>
2022-06-27 16:41:34 +0430

    adding items to basket

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,9 +1,79 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Basket, IBasket, IBasketItem } from '../shared/models/basket';
+import { IProduct } from '../shared/models/Products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<IBasket>(null);
+  basket$ = this.basketSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBasket(id: string){
+    return this.http.get(this.baseUrl + 'basket?id=' + id).pipe(
+      map((basket: any) => 
+      {
+        this.basketSource.next(basket);  
+      })
+    );
+  }
+
+  setBasket(basket: IBasket){
+    return this.http.post(this.baseUrl + 'basket' , basket).subscribe((response: any)=> {
+    this.basketSource.next(response);
+  }, error => {
+    console.log(error);
+    
+  });
+  }
+
+  getCurrentBasketValue(){
+    return this.basketSource.value;
+  }
+
+  addItem(item: IProduct, quantity =1){
+    const itemToAdd: IBasketItem = this.mapProductItemToBasketItem(item , quantity);
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.addOrUpdate(basket.items, itemToAdd, quantity);
+
+  }
+  private addOrUpdate(items: IBasketItem[], itemToAdd: IBasketItem, quantity: number): IBasketItem[] {
+    const index = items.findIndex(i => i.id === itemToAdd.id);
+    if(index === -1){
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    }
+    else{
+      items[index].quantity += quantity;
+
+    }
+
+    return items;
+  }
+  private createBasket(): IBasket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+  private mapProductItemToBasketItem(item: IProduct, quantity: number): IBasketItem {
+    return{
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      pictureUrl: item.pictureUrl,
+      quantity,
+      brand: item.productBrand,
+      type: item.productType
+    }
+  }
+
+
+
+  
 }
